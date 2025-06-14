89ab3b59272d9a77610254007d7fc5e5c2ddbd71
prajeeta pal <96904203+prajeeta15@users.noreply.github.com>
2025-06-14 20:09:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,6 @@ const IPO = mongoose.model("IPO", ipoSchema);
 
 app.get("/ipos", async (req, res) => res.json(await IPO.find()));
 app.post("/ipos", async (req, res) => res.json(await new IPO(req.body).save()));
+app.use("/ipos", require("./routes/ipoRoutes"));
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
